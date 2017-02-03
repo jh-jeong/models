0fc50a2ba09d4370a8e8d294dac043347aa0e361
Jongheon Jeong <jongheonj@kaist.ac.kr>
2017-02-04 04:10:44 +0900

    Fix cifar10 for compatibility on r0.12

--- tutorials/image/cifar10/cifar10_train.py
+++ tutorials/image/cifar10/cifar10_train.py
@@ -55,66 +55,66 @@ tf.app.flags.DEFINE_boolean('log_device_placement', False,
 
 
 def train():
-  """Train CIFAR-10 for a number of steps."""
-  with tf.Graph().as_default():
-    global_step = tf.contrib.framework.get_or_create_global_step()
+    """Train CIFAR-10 for a number of steps."""
+    with tf.Graph().as_default():
+        global_step = tf.contrib.framework.get_or_create_global_step()
 
-    # Get images and labels for CIFAR-10.
-    images, labels = cifar10.distorted_inputs()
+        # Get images and labels for CIFAR-10.
+        images, labels = cifar10.distorted_inputs()
 
-    # Build a Graph that computes the logits predictions from the
-    # inference model.
-    logits = cifar10.inference(images)
+        # Build a Graph that computes the logits predictions from the
+        # inference model.
+        logits = cifar10.inference(images)
 
-    # Calculate loss.
-    loss = cifar10.loss(logits, labels)
+        # Calculate loss.
+        loss = cifar10.loss(logits, labels)
 
-    # Build a Graph that trains the model with one batch of examples and
-    # updates the model parameters.
-    train_op = cifar10.train(loss, global_step)
+        # Build a Graph that trains the model with one batch of examples and
+        # updates the model parameters.
+        train_op = cifar10.train(loss, global_step)
 
-    class _LoggerHook(tf.train.SessionRunHook):
-      """Logs loss and runtime."""
+        class _LoggerHook(tf.train.SessionRunHook):
+            """Logs loss and runtime."""
 
-      def begin(self):
-        self._step = -1
+            def begin(self):
+                self._step = -1
 
-      def before_run(self, run_context):
-        self._step += 1
-        self._start_time = time.time()
-        return tf.train.SessionRunArgs(loss)  # Asks for loss value.
+            def before_run(self, run_context):
+                self._step += 1
+                self._start_time = time.time()
+                return tf.train.SessionRunArgs(loss)  # Asks for loss value.
 
-      def after_run(self, run_context, run_values):
-        duration = time.time() - self._start_time
-        loss_value = run_values.results
-        if self._step % 10 == 0:
-          num_examples_per_step = FLAGS.batch_size
-          examples_per_sec = num_examples_per_step / duration
-          sec_per_batch = float(duration)
+            def after_run(self, run_context, run_values):
+                duration = time.time() - self._start_time
+                loss_value = run_values.results
+                if self._step % 10 == 0:
+                    num_examples_per_step = FLAGS.batch_size
+                    examples_per_sec = num_examples_per_step / duration
+                    sec_per_batch = float(duration)
 
-          format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                        'sec/batch)')
-          print (format_str % (datetime.now(), self._step, loss_value,
-                               examples_per_sec, sec_per_batch))
+                    format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                                  'sec/batch)')
+                    print(format_str % (datetime.now(), self._step, loss_value,
+                                        examples_per_sec, sec_per_batch))
 
-    with tf.train.MonitoredTrainingSession(
-        checkpoint_dir=FLAGS.train_dir,
-        hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
-               tf.train.NanTensorHook(loss),
-               _LoggerHook()],
-        config=tf.ConfigProto(
-            log_device_placement=FLAGS.log_device_placement)) as mon_sess:
-      while not mon_sess.should_stop():
-        mon_sess.run(train_op)
+        with tf.train.MonitoredTrainingSession(
+                checkpoint_dir=FLAGS.train_dir,
+                hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
+                       tf.train.NanTensorHook(loss),
+                       _LoggerHook()],
+                config=tf.ConfigProto(
+                    log_device_placement=FLAGS.log_device_placement)) as mon_sess:
+            while not mon_sess.should_stop():
+                mon_sess.run(train_op)
 
 
 def main(argv=None):  # pylint: disable=unused-argument
-  cifar10.maybe_download_and_extract()
-  if tf.gfile.Exists(FLAGS.train_dir):
-    tf.gfile.DeleteRecursively(FLAGS.train_dir)
-  tf.gfile.MakeDirs(FLAGS.train_dir)
-  train()
+    cifar10.maybe_download_and_extract()
+    if tf.gfile.Exists(FLAGS.train_dir):
+        tf.gfile.DeleteRecursively(FLAGS.train_dir)
+    tf.gfile.MakeDirs(FLAGS.train_dir)
+    train()
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
